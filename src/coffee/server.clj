f307a7ec7d58560e69af388c8d26abb2a767ac26
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-05-29 23:15:15 +0200

    ws route

--- src/coffee/server.clj
+++ src/coffee/server.clj
@@ -6,7 +6,14 @@
 (def server (atom nil))
 
 (defroutes server-routes
-  (GET "/" [] "Hello World"))
+  (GET "/ws" []
+       (fn [req]
+         (with-channel req channel
+           (on-close channel (fn [status]
+                               (println "Channel closed " status)))
+           (on-receive channel (fn [data]
+                                 (println "Data " data))))))
+  (resources "public"))
 
 (defn start-server []
   (reset! server (run-server server-routes {:port 8080})))
