67c3df4be0080abf9279182c5a873f7353ad6bf8
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-05-30 00:41:07 +0200

    refactoring

--- src/coffee/server.clj
+++ src/coffee/server.clj
@@ -7,21 +7,33 @@
 
 (def open-connections (ref []))
 
+(defn remove-connection [channel]
+  (dosync
+   (alter open-connections
+          #(remove (partial = channel) %))))
+
+(defn save-connection [channel]
+  (dosync
+   (alter open-connections conj channel)))
+
+(defn send-to-all [connections data]
+  (doseq [con connections]
+    (send! con data)))
+
+(defn ws-handler [req]
+  (with-channel req channel
+    (save-connection channel)
+    (println "WS connection opened")
+    (on-close channel (fn [status]
+                        (remove-connection channel)
+                        (println "Channel closed " status)
+                        (println "Connections left " (count @open-connections))))
+    (on-receive channel (fn [data]
+                          (println "Data " data)
+                          (send-to-all @open-connections data)))))
+
 (defroutes server-routes
-  (GET "/ws" []
-       (fn [req]
-         (with-channel req channel
-           (dosync
-            (alter open-connections conj channel))
-           (println "WS connection opened")
-           (on-close channel (fn [status]
-                               (dosync
-                                (alter open-connections
-                                       #(remove (partial = channel) %)))
-                               (println "Channel closed " status)
-                               (println "Connections left " (count @open-connections))))
-           (on-receive channel (fn [data]
-                                 (println "Data " data))))))
+  (GET "/ws" [] ws-handler)
   (resources "public"))
 
 (defn start-server []
