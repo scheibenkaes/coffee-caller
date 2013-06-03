9001164f7d25d611c32622f77c9aa2da8a7e2467
Benjamin Klüglein <scheibenkaes@googlemail.com>
2013-06-03 21:17:46 +0200

    kind of working html setup

--- resources/public/app.js
+++ resources/public/app.js
@@ -1,2 +1,29 @@
-function Controller($scope){    
+function calcLightState(oldState){
+    return oldState;
+}
+
+function Controller($scope){        
+    $scope.light = null;
+
+    $scope.$watch('light', function(newValue, oldValue){
+        if(!newValue) {
+            angular.forEach(['green', 'red', 'yellow'], function(col){
+                document.getElementById(col).className = null;
+            });
+        } else {
+            angular.element('#' + newValue).addClass("lit");
+//            document.getElementById(newValue).className = "lit";
+        }
+    });
+
+    $scope.sendRequest = function(){
+        $scope.light = "red";
+    };
+    
+    var ws = new WebSocket("ws://localhost:8080/ws");
+    ws.onmessage = function(msg){
+        console.log(msg.data);
+        $scope.light = calcLightState(msg.data.trim());
+        $scope.$digest();
+    };
 }
